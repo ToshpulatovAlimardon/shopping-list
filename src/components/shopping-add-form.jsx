c49fb6249042663c126e2446703f45175420c4eb
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-07-09 18:37:11 +0500

    added onChange

--- src/components/shopping-add-form.jsx
+++ src/components/shopping-add-form.jsx
@@ -1,11 +1,54 @@
-const ShoppingAddForm = () => {
-  return (
-    <div className="form">
-      <input type="text" className="title" placeholder="Title..." />
-      <input type="number" className="number" placeholder="14" />
-      <button>Add</button>
-    </div>
-  );
-};
+import React from "react";
+
+class ShoppingAddForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Buy ",
+      number: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onAdd = () => {
+    const data = {
+      title: this.state.title,
+      number: this.state.number,
+    };
+
+    console.log(data);
+  };
+
+  render() {
+    const { number, title } = this.state;
+
+    return (
+      <div className="form">
+        <input
+          type="text"
+          className="title"
+          placeholder="Title..."
+          name="title"
+          onChange={this.onChange}
+          value={title}
+        />
+        <input
+          type="number"
+          className="number"
+          placeholder="14"
+          name="number"
+          onChange={this.onChange}
+          value={number}
+        />
+        <button onClick={this.onAdd}>Add</button>
+      </div>
+    );
+  }
+}
 
 export default ShoppingAddForm;
